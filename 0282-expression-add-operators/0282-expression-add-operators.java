c9f0688a716a3f17849e62cbdc86d49e2aee92e3
Atul Patel <atulrpatel007@gmail.com>
2026-06-26 23:00:04 +0530

    Time: 128 ms (52.91%), Space: 52.9 MB (56.12%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.java
+++ 0282-expression-add-operators/0282-expression-add-operators.java
@@ -1,31 +1,81 @@
 class Solution {
     public List<String> addOperators(String num, int target) {
-        List<String> rst = new ArrayList<String>();
-        if(num == null || num.length() == 0) return rst;
-        helper(rst, "", num, target, 0, 0, 0);
-        return rst;
-    }
-    public void helper(List<String> rst, String path, String num, int target, int pos, long eval, long multed){
-        if(pos == num.length()){
-            if(target == eval)
-                rst.add(path);
-            return;
+            List<String> ans = new ArrayList<>();
+            backtrack(num, target, 0, 0, 0, "", ans);
+            return ans;
         }
-        for(int i = pos; i < num.length(); i++){
-            if(i != pos && num.charAt(pos) == '0') break;
-            long cur = Long.parseLong(num.substring(pos, i + 1));
-            if(pos == 0){
-                helper(rst, path + cur, num, target, i + 1, cur, cur);
+        static void backtrack(String num,
+                               int target,
+                               int index,
+                               long currValue,
+                               long lastOperand,
+                               String expr,
+                               List<String> ans) {
+
+            if (index == num.length()) {
+                if (currValue == target) {
+                    ans.add(expr);
+                }
+                return;
             }
-            else{
-                helper(rst, path + "+" + cur, num, target, i + 1, eval + cur , cur);
-                
-                helper(rst, path + "-" + cur, num, target, i + 1, eval -cur, -cur);
-                
-                helper(rst, path + "*" + cur, num, target, i + 1, eval - multed + multed * cur, multed * cur );
+
+            for (int i = index; i < num.length(); i++) {
+
+                // Avoid numbers with leading zeros
+                if (i > index && num.charAt(index) == '0') {
+                    break;
+                }
+
+                String part = num.substring(index, i + 1);
+                long value = Long.parseLong(part);
+
+                if (index == 0) {
+
+                    backtrack(
+                            num,
+                            target,
+                            i + 1,
+                            value,
+                            value,
+                            part,
+                            ans
+                    );
+
+                } else {
+
+                    // Addition
+                    backtrack(
+                            num,
+                            target,
+                            i + 1,
+                            currValue + value,
+                            value,
+                            expr + "+" + part,
+                            ans
+                    );
+
+                    // Subtraction
+                    backtrack(
+                            num,
+                            target,
+                            i + 1,
+                            currValue - value,
+                            -value,
+                            expr + "-" + part,
+                            ans
+                    );
+
+                    // Multiplication
+                    backtrack(
+                            num,
+                            target,
+                            i + 1,
+                            currValue - lastOperand + lastOperand * value,
+                            lastOperand * value,
+                            expr + "*" + part,
+                            ans
+                    );
+                }
             }
         }
-    }
 }
-        
-   
